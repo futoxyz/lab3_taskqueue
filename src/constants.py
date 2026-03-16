19e84a6c80f5d2f505d85c01f3e883d0254181cb
futo <fedor.alekseev.msk@gmail.com>
2026-03-16 19:58:10 +0300

    Add niche playfield

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,3 @@
 STATUS_LIST: list[str] = ["pending", "in_progress", "failed", "completed"]
 DESCRIPTIONS: list[str] = ["Starts...", "Reloads...", "Stops...", "Rebuilds...", "Hashing...", "Resets...", "Give permission..."]
+COMMANDS: list[str] = ["add-task", "show-tasks", "change-task-status"]
--- src/main.py
+++ src/main.py
@@ -1,16 +1,39 @@
-from src.source import RandomSource, APISource
-from src.task import TaskGiver
+from src.source import RandomSource, TaskGiver
+from src.constants import COMMANDS
+import shlex
 
 
 def main() -> None:
+    print("Available commands:\n1. add-task\n2. show-tasks\n3. change-task-status")
+    task_list: list[Task] = []
     while inp := input():
-        if not inp.isdigit(): continue
-        tasks_rnd = RandomSource(int(inp))
-        tasks_api = APISource(int(inp))
-        if isinstance(tasks_rnd, TaskGiver) and isinstance(tasks_api, TaskGiver):
-            tasks_rnd.get_tasks()
-            tasks_api.get_tasks()
-            print("\"srcs.log\" updated")
+        if inp not in COMMANDS: continue
+        match inp:
+            case "add-task":
+                tasks_rnd = RandomSource(1)
+                if isinstance(tasks_rnd, TaskGiver):
+                    task_list += tasks_rnd.get_tasks()
+                    print("Initiated task with generator")
+            case "show-tasks":
+                if not task_list:
+                    print("No active tasks")
+                else:
+                    for task in task_list:
+                        print(f"{task.id}: {task.description}. Priority - {task.priority}, status: {task.status}")
+            case "change-task-status":
+                print("Enter the task id")
+                id = str(input())
+                task_exists = False
+                for task in task_list:
+                    if task.id == id:
+                        print("Enter new status")
+                        new_status = str(input())
+                        task.status = new_status
+                        task_exists = True
+                        break
+                if not task_exists: raise ValueError("No such task")
+
+
 
 
 if __name__ == "__main__":
